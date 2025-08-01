475fd4a7b6f52bb3c51a8624c530922755588967
Palmovitsch0 <martinmueller1983@posteo.com>
2025-08-01 17:09:27 +0200

    Pipes deleted

--- firstTry/Assets/PipeSpawnScript.cs
+++ firstTry/Assets/PipeSpawnScript.cs
@@ -5,11 +5,12 @@ public class PipeSpawnScript : MonoBehaviour
     public GameObject pipe;
     public float spawnRate = 2;
     private float timer = 0;
+    public float heightOffset = 10;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        spawnPipe();
     }
 
     // Update is called once per frame
@@ -19,6 +20,18 @@ public class PipeSpawnScript : MonoBehaviour
         {
             timer += Time.deltaTime;
         }
-        Instantiate(pipe, transform.position, transform.rotation)
+        else
+        {
+            spawnPipe();
+            timer = 0;
+        }
+    }
+
+    void spawnPipe()
+    {
+        float lowestPoint = transform.position.y - heightOffset;
+        float highestPoint = transform.position.y + heightOffset;
+
+        Instantiate(pipe, new Vector3(transform.position.x, Random.Range(lowestPoint, highestPoint), 0), transform.rotation);
     }
 }
